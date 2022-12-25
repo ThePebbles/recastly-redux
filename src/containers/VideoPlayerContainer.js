58b5ef7dfe9ba16723f86f127d5736b74b7a35a2
Alvino Handiman <alvinohandiman004@gmail.com>
2022-12-24 22:24:35 -0500

    Working on containers

--- src/containers/VideoPlayerContainer.js
+++ src/containers/VideoPlayerContainer.js
@@ -1,9 +1,26 @@
 import { connect } from 'react-redux';
 import VideoPlayer from '../components/VideoPlayer.js';
+import changeVideo from './../actions/currentVideo.js';
+import currentVideoReducer from './../reducers/currentVideo.js';
+import searchYouTube from '../lib/searchYouTube.js';
+import YOUTUBE_API_KEY from '../config/youtube.js';
 
-var VideoPlayerContainer = () => {};
+const mapStateToProps = (state) => {
+  return {
+    video: state.video
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return (dispatch) => {
+    return searchYouTube({YOUTUBE_API_KEY}, (data) => {
+      dispatch(changeVideo(data[0]));
+    });
+  };
+};
+
+
 
 //TODO: define a VideoPlayerContainer component which will hook up your action
 //dispatchers with your VideoPlayer component props.
 
-export default VideoPlayerContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(VideoPlayer);
