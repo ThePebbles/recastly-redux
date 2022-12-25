2e9932bf934d2bd936110b2ef76e92eb6b049ef0
Alvino Handiman <alvinohandiman004@gmail.com>
2022-12-24 20:22:01 -0500

    Complete planning state

--- src/store/store.js
+++ src/store/store.js
@@ -4,3 +4,4 @@ import rootReducer from './../reducers/main.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
+//states: videos (an array in app), currentVideo (set to null, maybe object?, in app), value (set to quotes, in search)
